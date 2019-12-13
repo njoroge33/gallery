e429ca7333e68443fbc28fb82cd58cfb2f103673
njoroge33 <johngichuhi769@gmail.com>
2019-12-13 19:04:59 +0300

    create  category and location models

--- image/models.py
+++ image/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
